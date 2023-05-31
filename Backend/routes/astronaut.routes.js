8194a4c5e8fc403541adceaabb686e67f5230336
NicolasMiladinovic <nicolas.miladinovic@gmail.com>
2023-05-31 23:48:04 +0200

    [FEAT] Create routes

--- Backend/routes/astronaut.routes.js
+++ Backend/routes/astronaut.routes.js
@@ -4,5 +4,8 @@ const astronautController = require('../controllers/astronaut.controller.js')
 const router = express.Router()
 
 router.get('/getAstronauts', astronautController.getAstronauts)
+router.post('/addAstronaut', astronautController.addAstronaut)
+router.put('/updateAstronaut/:id', astronautController.updateAstronaut)
+router.delete('/deleteAstronaut/:id', astronautController.deleteAstronaut)
 
 module.exports = router
