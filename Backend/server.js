39454470750a92e30b88dddb31d093f280687ab4
NicolasMiladinovic <nicolas.miladinovic@gmail.com>
2023-06-01 00:20:10 +0200

    [FEAT] Add body-parser

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const astronautRoutes = require('./routes/astronaut.routes.js')
 
 const app = express()
 
 const PORT = 3000
 
+// Authorize all requests
 app.use(cors())
+// Authorize json requests
+app.use(bodyParser.json());
+// Define routes
 app.use('/', astronautRoutes)
 
 app.listen(PORT, () => {
