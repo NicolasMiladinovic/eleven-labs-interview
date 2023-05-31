61f4713ff86da0f3b2def0447560a91a009874f8
NicolasMiladinovic <nicolas.miladinovic@gmail.com>
2023-05-31 23:03:30 +0200

    [FEAT] Add cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const astronautRoutes = require('./routes/astronaut.routes.js')
 
 const app = express()
 
 const PORT = 3000
 
+app.use(cors())
 app.use('/', astronautRoutes)
 
 app.listen(PORT, () => {
